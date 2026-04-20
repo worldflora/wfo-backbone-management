49422d21599be14fe8d9fd64b7c016168f9a7ca8
Roger <rhyam@rbge.org.uk>
2026-04-20 10:53:22 +0100

    hard code db name

--- scripts/db_backup_live.sh
+++ scripts/db_backup_live.sh
@@ -1,6 +1,6 @@
 # will backup the live database as specified in the config file.
 # suitable for being run by cron
-databasename=$(php db_live.php)
+databasename="promethius"
 today=$(date +"%Y-%m-%d-%H-%M-%S")
 filename="../data/db_dumps/${databasename}_${today}.sql"
 mysqldump $databasename > $filename
